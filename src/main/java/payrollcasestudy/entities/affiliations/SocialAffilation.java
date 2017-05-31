20c5bb2cdebddb20c84a8a161d8044c10007e9b0
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-05-31 09:29:14 -0400

    Creando interfaz Affilation
    Creando SocialAffilation

---
+++ src/main/java/payrollcasestudy/entities/affiliations/SocialAffilation.java
@@ -0,0 +1,9 @@
+package payrollcasestudy.entities.affiliations;
+
+public class SocialAffilation implements Affiliation {
+
+	public SocialAffilation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
